f0b5f23568ab9531d39e347dbcc574a6ae361ddc
yxc775 <yxc775@case.edu>
2019-11-19 22:07:32 -0500

    fixing printing execution sequential issue

--- src/risingCity.java
+++ src/risingCity.java
@@ -3,10 +3,13 @@ import java.io.*;
 public class risingCity {
     MinHeap minheap = new MinHeap();
     RBTree rbTree = new RBTree();
-    public void insert(int buildingnum,int totaltime){
-        BuildingInfo item = new BuildingInfo(buildingnum,0,totaltime);
+    public void insert(BuildingInfo item){
         RBNode rbn = new RBNode(item);
         rbTree.insert(rbn);
+        minheap.insert(item);
+    }
+
+    public void update(BuildingInfo item){
         minheap.insert(item);
     }
 
@@ -30,6 +33,27 @@ public class risingCity {
             }
     }
 
+    public void executecommand(String[] command, StringBuilder res){
+        String[] inputs = command[1].trim().replaceAll("[()]", " ").split(" ");
+        switch(inputs[0]) {
+            case "Insert":
+                String[] parameters = inputs[1].split(",");
+                BuildingInfo item = new BuildingInfo(Integer.parseInt(parameters[0]),0, Integer.parseInt(parameters[1]));
+                insert(item);
+                break;
+            case "PrintBuilding":
+                String[] buildingnum = inputs[1].split(",");
+                if (buildingnum.length == 1) {
+                    res.append(print(Integer.parseInt(buildingnum[0])) + "\n");
+                } else {
+                    res.append(print(Integer.parseInt(buildingnum[0]), Integer.parseInt(buildingnum[1])) + "\n");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
     public static void main(String[] args){
         BufferedReader reader = null;
 
@@ -46,61 +70,38 @@ public class risingCity {
             String line = reader.readLine();
             risingCity interpretor = new risingCity();
             StringBuilder res = new StringBuilder();
-            while(line != null){
-                String[] command = line.split(":");
-                //do something here
-                while(Integer.parseInt(command[0]) > globalCounter){
-                    globalCounter += 1;
-                    if(interpretor.minheap.peek()!= null){
-                        cumulativeincrement += 1;
-                        BuildingInfo mayUpdate = interpretor.minheap.peek();
-                        if (mayUpdate.getE() + cumulativeincrement >= mayUpdate.getT()) {
-                            res.append("(" + mayUpdate.getB() +"," +globalCounter + ")" + "\n");
-                            interpretor.minheap.pop();
-                            interpretor.rbTree.delete(mayUpdate.point);
-                            cumulativeincrement = 0;
-                        }
-                        else if(cumulativeincrement >= 5){
-                            interpretor.minheap.update(5);
-                            cumulativeincrement = 0;
-                        }
+            BuildingInfo curB = null;
+            while(curB != null || line != null || interpretor.minheap.peek() != null){
+                if(line != null) {
+                    String[] command = line.split(":");
+                    if(Integer.parseInt(command[0]) == globalCounter){
+                        interpretor.executecommand(command,res);
+                        line = reader.readLine();
                     }
                 }
-                String[] inputs = command[1].trim().replaceAll("[()]", " ").split(" ");
-                switch(inputs[0]){
-                    case "Insert":
-                        String[] parameters = inputs[1].split(",");
-                        interpretor.insert(Integer.parseInt(parameters[0]),Integer.parseInt(parameters[1]));
-                        break;
-                    case "PrintBuilding":
-                        String[] buildingnum = inputs[1].split(",");
-                        if(buildingnum.length == 1){
-                            res.append(interpretor.print(Integer.parseInt(buildingnum[0])) + "\n");
-                        }
-                        else{
-                            res.append(interpretor.print(Integer.parseInt(buildingnum[0]),Integer.parseInt(buildingnum[1]))+"\n");
-                        }
-                        break;
-                    default:
-                        break;
-                }
-                line = reader.readLine();
-            }
 
-            while(interpretor.minheap.peek() != null){
-                BuildingInfo mayUpdate = interpretor.minheap.peek();
-                cumulativeincrement += 1;
-                globalCounter += 1;
-                if (mayUpdate.getE() + cumulativeincrement >= mayUpdate.getT()) {
-                    res.append("(" + mayUpdate.getB() +"," +globalCounter+ ")" + "\n");
-                    interpretor.minheap.pop();
-                    interpretor.rbTree.delete(mayUpdate.point);
-                    cumulativeincrement = 0;
+                if(curB != null || interpretor.minheap.peek() != null){
+                    if(curB != null && curB.getE() == curB.getT()){
+                        interpretor.rbTree.delete(curB.point);
+                        res.append("(" + curB.getB() +"," +globalCounter+ ")" + "\n");
+                        curB = null;
+                        cumulativeincrement = 0;
+                    }
+                    else if(cumulativeincrement == 5){
+                        interpretor.update(curB);
+                        cumulativeincrement = 0;
+                        curB = null;
+                    }
+                    if(curB != null || interpretor.minheap.peek() != null){
+                        if(cumulativeincrement == 0){
+                            curB = interpretor.minheap.pop();
+                        }
+                        curB.setE(curB.getE() + 1);
+                        curB.point.setE(curB.getE());
+                        cumulativeincrement++;
+                    }
                 }
-                else if(cumulativeincrement >= 5){
-                    interpretor.minheap.update(5);
-                    cumulativeincrement = 0;
-                }
+                globalCounter++;
             }
 
 
